25fbc2314b89afc5df52d9ec46f0239ce2dca85b
Abuenameh Aiyejina <abuenameh@gmail.com>
2022-08-01 23:43:02 -0400

    Cleanup snapshots

--- howdy/src/snapshot.hh
+++ howdy/src/snapshot.hh
@@ -6,17 +6,6 @@
 
 #include <opencv2/core.hpp>
 
-// class snapshot {
-//     public:
-//     snapshot(const std::string& path_);
-
-// 	/* Generate a shapshot from given frames */
-//     std::string generate(std::vector<cv::Mat>& frames, std::vector<std::string>& text_lines);
-
-//     private:
-//     const std::string PATH;
-// };
-
-    std::string generate(std::vector<cv::Mat>& frames, std::vector<std::string>& text_lines);
+std::string generate(std::vector<cv::Mat> &frames, std::vector<std::string> &text_lines);
 
 #endif // SNAPSHOT_H_
