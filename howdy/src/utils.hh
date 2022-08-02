22dd0538bdc179b18f36e27bfbdd92d3a5f40938
Abuenameh Aiyejina <abuenameh@gmail.com>
2022-08-01 21:42:41 -0400

    Refactor VideoCapture

---
+++ howdy/src/utils.hh
@@ -0,0 +1,7 @@
+#ifndef UTILS_H_
+#define UTILS_H_
+
+void exit_code(int code);
+
+
+#endif // UTILS_H_
--- howdy/src/video_capture.hh
+++ howdy/src/video_capture.hh
@@ -3,17 +3,50 @@
 
 #include <string>
 
+#include <opencv2/videoio.hpp>
+
 #include <INIReader.h>
 
-class VideoCapture {
+class VideoCapture
+{
 
 public:
+    /*
+    Creates a new VideoCapture instance depending on the settings in the
+    provided config file.
+    */
+    VideoCapture(INIReader config_);
 
-    VideoCapture(std::string configFile);
-    VideoCapture(INIReader contig);
+    /*
+    Frees resources when destroyed
+    */
+    ~VideoCapture();
 
-    private:
-    
-}
+    /*
+    Release cameras
+    */
+    void release();
+
+    /*
+    Reads a frame, returns the frame and an attempted grayscale conversion of
+    the frame in a tuple:
+
+    (frame, grayscale_frame)
+
+    If the grayscale conversion fails, both items in the tuple are identical.
+    */
+    void read_frame(cv::Mat &frame, cv::Mat &gsframe);
+
+    double get(int propId);
+
+    bool set(int propId, double value);
+
+    int fw;
+    int fh;
+
+private:
+    INIReader config;
+    cv::VideoCapture internal;
+};
 
 #endif // VIDEO_CAPTURE_H_
