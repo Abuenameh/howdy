b4996491b12ebd040785f08e262da1cf08a29367
Abuenameh Aiyejina <abuenameh@gmail.com>
2022-08-01 22:19:41 -0400

    Refactor models

--- howdy/src/utils.hh
+++ howdy/src/utils.hh
@@ -1,7 +1,13 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+#include <opencv2/videoio.hpp>
+#include <dlib/opencv.h>
+
+using namespace dlib;
+
 void exit_code(int code);
 
+void convert_image(cv::Mat &iimage, matrix<rgb_pixel> &oimage);
 
 #endif // UTILS_H_
